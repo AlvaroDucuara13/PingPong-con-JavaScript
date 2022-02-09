bf0c6b6f7789eeca8d743ce22e8d61032e97d3ad
Alvaro Ducuara <alvaro.isledier@gmail.com>
2022-02-08 21:48:45 -0500

    Se agrego las coliciones de las barras contra la pelota de Ping Pong vistos en la practica MVC_6

--- Js/main.js
+++ Js/main.js
@@ -33,19 +33,43 @@
         this.speed_y = 0;
         this.speed_x = 3;
         this.direction = 1;
+        this.bounce_angle=0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
 
         this.board = board;
         board.ball = this;
         this.kind = "circle"
-        
 
-        
     }
     self.Ball.prototype={
             move : function(){
                 this.x += (this.speed_x * this.direction);
                 this.y += (this.speed_y);
-            }
+            },
+            get width(){
+                return this.radius*2;
+            },
+            get height(){
+                return this.radius*2;
+            },
+            collisions: function(bar){
+        
+                //reacciona a una colision con una barra que recibe como parametro
+                // calcula el angulo en el que va a moverse la pelota
+                // modifica la dirección dependiendo de la dirección de la barra
+                var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+
+                var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+                this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+                this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+                this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+                if (this.x > (this.board.width / 2)) this.direction = -1;
+                else this.direction = 1;
+                }
         }
 })();
 
@@ -104,14 +128,47 @@
             };
         },
 
+        check_collisions: function(){
+            for (var i = this.board.bars.length - 1; i >= 0; i--){
+                var bar = this.board.bars[i];
+                if(hit(bar, this.board.ball)){
+                    this.board.ball.collisions(bar);
+                }
+            };
+        },
+
         play: function(){
             if(this.board.playing){
                 this.clean();
                 this.draw();
+                this.check_collisions();
                 this.board.ball.move();
             }
            
         }
+    }
+
+    function hit(a,b){
+        //Revisa si a coliciona con b
+        var hit = false;
+		//Colsiones horizontales
+		if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+			//Colisiones verticales
+			if (b.y + b.height >= a.y && b.y < a.y + a.height)
+				hit = true;
+		}
+		//Colisión de a con b
+		if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+			if (b.y <= a.y && b.y + b.height >= a.y + a.height)
+				hit = true;
+		}
+		//Colisión b con a
+		if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+			if (a.y <= b.y && a.y + a.height >= b.y + b.height)
+				hit = true;
+		}
+		return hit;
+
     }
 
     function draw(ctx, element){
